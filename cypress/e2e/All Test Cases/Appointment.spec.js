25be25c693cfe5d544029eeeb317e021b54b2fa0
Peter Abah <peterabah1819@gmail.com>
2023-05-29 11:25:28 +0100

    another commit

--- cypress/e2e/All Test Cases/Appointment.spec.js
+++ cypress/e2e/All Test Cases/Appointment.spec.js
@@ -85,4 +85,70 @@ it('Create an appointment', function()
 
 
 
+
+
+
+
+
+
+
+
+
+
+    it('Filter Appointments', function()
+    {
+        cy.login()
+        //click on appointment
+        cy.get('[ng-href="/appointments"]').click()
+        //Click on Add appointment
+        cy.get('.Appointments__subheader > :nth-child(1) > :nth-child(1)').click()
+        //search for a registered patient
+        cy.get('.AlmightyPatient__label > .multiselect > .multiselect__tags').click()
+        //Select the test patient
+        cy.get(':nth-child(3) > .multiselect__option > .AlmightyPatient__option').click()
+        //search for a doctor
+        cy.get('.AlmightyDoctor__label > .multiselect > .multiselect__tags').click().type('Test Test').wait(2000)
+        //Select the doctor
+        cy.get('.AlmightyDoctor__label > .multiselect > .multiselect__content-wrapper > .multiselect__content > :nth-child(1) > .multiselect__option').click()
+        //select date
+      cy.get('.toast-message').contains('Request failed with status code 422')
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    it('Refresh Calender', function()
+    {
+        cy.login()
+        //click on appointment
+        cy.get('[ng-href="/appointments"]').click()
+        //Click on Add appointment
+        cy.get('.Appointments__subheader > :nth-child(1) > :nth-child(1)').click()
+        //search for a registered patient
+        cy.get('.AlmightyPatient__label > .multiselect > .multiselect__tags').click()
+        //Select the test patient
+        cy.get(':nth-child(3) > .multiselect__option > .AlmightyPatient__option').click()
+        //search for a doctor
+        cy.get('.AlmightyDoctor__label > .multiselect > .multiselect__tags').click().type('Test Test').wait(2000)
+        //Select the doctor
+        cy.get('.AlmightyDoctor__label > .multiselect > .multiselect__content-wrapper > .multiselect__content > :nth-child(1) > .multiselect__option').click()
+        //select date
+      cy.get('.toast-message').contains('Request failed with status code 422')
+
+    })
+
+
 })
